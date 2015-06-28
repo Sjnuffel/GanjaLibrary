a38a95bd01e35bd94a2a2a7b9aba24fcb3b5f519
Sjnuffel <info@fort-wendy.com>
2015-06-28 16:41:07 +0200

    First commit of IChronic, interface for plant growth.

---
+++ GanjaTestApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using GanjaLibrary.Interfaces;
+using GanjaLibrary.Classes;
+using GanjaLibrary.Enums;
+
+namespace GanjaTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IChronic GanjaGame = new Sativa();
+            GanjaGame.Print();
+            Console.WriteLine();
+            for (int i = 0; i < 7; i++)
+            {
+                GanjaGame.Grow(Water.Low, Light.Off, Food.None);
+                GanjaGame.Print();
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                GanjaGame.Grow(Water.Low, Light.Spring, Food.None);
+                GanjaGame.Print();
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                GanjaGame.Grow(Water.Low, Light.Summer, Food.None);
+                GanjaGame.Print();
+                Console.WriteLine();
+            }
+
+            GanjaGame.Print();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
