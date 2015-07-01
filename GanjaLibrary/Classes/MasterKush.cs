edfeef70c945808b3b0158dd2e241f88d7483bf9
Sjnuffel <info@fort-wendy.com>
2015-07-01 21:30:45 +0200

    Added some new Weed classes to test with.

---
+++ GanjaLibrary/Classes/MasterKush.cs
@@ -0,0 +1,41 @@
+﻿using GanjaLibrary.Enums;
+using System.Collections.Generic;
+
+namespace GanjaLibrary.Classes
+{
+    // Master Kush is an example of an almost 100% Indica plant.
+    public class MasterKush : Indica
+    {
+        public MasterKush() : base()
+        {
+            Name = "Master Kush";
+            SeedingAge = 4;
+
+            CBD = 0.25;
+            THC = 0.30;
+
+            // Create a dict for changing water need per stage.
+            WaterNeed = new Dictionary<Stage, Water>()
+            {
+                { Stage.Seed, Water.Low },
+                { Stage.Clone, Water.Low },
+                { Stage.Vegetative, Water.Low },
+                { Stage.Flowering, Water.Medium },
+                { Stage.Dead, Water.Nothing },
+            };
+
+            // Create a dict for changing light need per stage.
+            LightNeed = new Dictionary<Stage, Light>()
+            {
+                { Stage.Seed, Light.Off },
+                { Stage.Clone, Light.Spring },
+                { Stage.Vegetative, Light.Spring },
+                { Stage.Flowering, Light.Summer },
+                { Stage.Dead, Light.Off },
+            };
+
+            Light = LightNeed[Stage.Seed];
+            Water = WaterNeed[Stage.Seed];
+        }
+    }
+}
---
+++ GanjaLibrary/Classes/SilverHaze.cs
@@ -0,0 +1,41 @@
+﻿using GanjaLibrary.Enums;
+using System.Collections.Generic;
+
+namespace GanjaLibrary.Classes
+{
+    // Silver Haze is an example of a near 100% Sativa plant.
+    public class SilverHaze : Sativa
+    {
+        public SilverHaze() : base()
+        {
+            Name = "Silver Haze";
+            SeedingAge = 6;
+
+            CBD = 0.05;
+            THC = 0.35;
+
+            // Create a dict for changing water need per stage.
+            WaterNeed = new Dictionary<Stage, Water>()
+            {
+                { Stage.Seed, Water.Low },
+                { Stage.Clone, Water.Low },
+                { Stage.Vegetative, Water.Low },
+                { Stage.Flowering, Water.Medium },
+                { Stage.Dead, Water.Nothing },
+            };
+
+            // Create a dict for changing light need per stage.
+            LightNeed = new Dictionary<Stage, Light>()
+            {
+                { Stage.Seed, Light.Off },
+                { Stage.Clone, Light.Spring },
+                { Stage.Vegetative, Light.Spring },
+                { Stage.Flowering, Light.Summer },
+                { Stage.Dead, Light.Off },
+            };
+
+            Light = LightNeed[Stage.Seed];
+            Water = WaterNeed[Stage.Seed];
+        }
+    }
+}
