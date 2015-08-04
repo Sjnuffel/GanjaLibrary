0ad8f33a50d1226c411287f05711737bd0f3b3fa
Guus Bouts <info@fort-wendy.com>
2015-08-04 14:21:50 +0200

    Trousers.cs wasn't being synced for some reason. Created CargoPants.cs as an alternative and remade Trousers.cs. Now it's synched and tests are still passing.

---
+++ GanjaLibrary/Classes/Items/CargoPants.cs
@@ -0,0 +1,52 @@
+﻿using GanjaLibrary.Interfaces.Items;
+using System.Collections.Generic;
+
+namespace GanjaLibrary.Classes.Items
+{
+    public class CargoPants : Item, IContainer
+    {
+        public List<IItem> Items { get; set; }
+
+        public int Slots { get; internal set; }
+        public int ItemAmount
+        {
+            get
+            { 
+                if (Items == null) return 0;
+                else return Items.Count;
+            }
+        }
+
+        public CargoPants() : base("Cargopants", "Pockets everywhere! Imagine the blunts you could hide in there.", 3, 250)
+        {
+            Items = new List<IItem>();
+            Slots = 8;
+        }
+
+        public bool Add(IItem item)
+        {
+            if (ItemAmount != Slots)
+            {
+                Items.Add(item);
+                if (Items.Contains(item))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public bool Remove(IItem item)
+        {
+            return Items.Remove(item);
+        }
+
+        public double Sell(IItem item)
+        {
+            double retVal = item.Value;
+            if (Remove(item))
+                return retVal;
+            else
+                return 0;
+        }
+    }
+}
