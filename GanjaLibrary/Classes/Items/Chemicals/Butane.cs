b0c954832da85c9803ca5075dfb009d98287271c
Guus Bouts <info@fort-wendy.com>
2015-08-08 17:08:53 +0200

    Trying to add a catalyst into the game for creating Oils.

---
+++ GanjaLibrary/Classes/Items/Chemicals/Butane.cs
@@ -0,0 +1,14 @@
+﻿
+namespace GanjaLibrary.Classes.Items.Chemicals
+{
+    class Butane : Item
+    {
+        public Butane() :base()
+        {
+            Name = "Butane";
+            Description = "Lighter Fluid";
+            Weight = 0.5;
+            Value = 25;
+        }
+    }
+}
