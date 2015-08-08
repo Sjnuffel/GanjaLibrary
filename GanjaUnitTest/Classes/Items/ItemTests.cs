a77d39c505555f2245a226d5c68ab2c9d2ffeb84
Guus Bouts <info@fort-wendy.com>
2015-08-08 20:44:18 +0200

    Added a test for items, started with Butane being added to the Inventory.

---
+++ GanjaUnitTest/Classes/Items/ItemTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GanjaLibrary.Interfaces;
+using GanjaLibrary.Enums;
+using GanjaLibrary.Interfaces.Items;
+using GanjaLibrary.Classes.Items;
+
+namespace GanjaUnitTest.Classes.Items
+{
+    [TestClass()]
+    public class ItemTests
+    {
+        [TestMethod()]
+        public void AddButaneToShopTest()
+        {
+            IContainer ShopTest = new Shop();
+            Item Butane = new Butane();
+            ShopTest.Add(Butane);
+
+            Assert.IsTrue(ShopTest.ItemAmount == 1);
+        }
+    }
+}
