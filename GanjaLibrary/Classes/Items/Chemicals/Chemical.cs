12648302b2ed8953b71fce60754cfbd395fc6489
Guus Bouts <info@fort-wendy.com>
2015-08-14 19:14:42 +0200

    Created an interface for chemicals.

---
+++ GanjaLibrary/Classes/Items/Chemicals/Chemical.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+    We can assume this is the base chemical every chemical is based off of.
+
+*/
+
+using GanjaLibrary.Interfaces.Items;
+
+namespace GanjaLibrary.Classes.Items.Chemicals
+{
+    public class Chemical : Item, IChemical
+    {
+        public double Flashpoint { get; set; }
+        public double Contents { get; set; }
+
+        public bool Flammable { get; set; }
+
+        // Inherit from Item and input name, description, weight and value in constructor.
+        public Chemical() :base(string.Empty, string.Empty, 1, 0)
+        {
+            Flashpoint = 100;
+            Contents = 1000;
+            Flammable = false;
+        }
+    }
+}
