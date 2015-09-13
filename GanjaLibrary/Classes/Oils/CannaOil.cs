b5644fc3c9309aa1d82cd729244e270bb0cd1238
Guus Bouts <info@fort-wendy.com>
2015-09-14 01:29:45 +0800

    CannaOil is now a partial class, similar to SolventMix and Chronic.

--- GanjaLibrary/Classes/Oils/CannaOil.cs
+++ GanjaLibrary/Classes/Oils/CannaOil.cs
@@ -1,8 +1,6 @@
 ﻿using GanjaLibrary.Interfaces;
 using System;
-using System.Collections.Generic;
 using GanjaLibrary.Enums;
-using GanjaLibrary.Interfaces.Items;
 using GanjaLibrary.Interfaces.Oils;
 
 namespace GanjaLibrary.Classes.Oils
@@ -11,154 +9,24 @@ namespace GanjaLibrary.Classes.Oils
     {
         private IChronic Chronic { get; set; }
 
-        public int Age {
-            get { return Chronic.Age; }
-        }
-
-        public double CBD {
-            get { return Chronic.CBD; }
-        }
-
-        public int DryingAge {
-            get { return Chronic.DryingAge; }
-        }
-
-        public int FloweringAge {
-            get { return Chronic.FloweringAge; }
-        }
-
-        public double Health {
-            get { return Chronic.Health; }
-        }
-
-        public double Height {
-            get { return Chronic.Height; }
-
-            set { Chronic.Height = value; }
-        }
-
-        public int MaxHealth {
-            get { return Chronic.MaxHealth; }
-        }
-
-        public double MaxYield {
-            get { return Chronic.MaxYield; }
-        }
-
-        public string Name {
-            get { return Chronic.Name; }
-        }
-
-        public double Quality {
-            get { return Chronic.Quality; }
-
-            set { Chronic.Quality = value; }
-        }
-
-        public int SeedingAge {
-            get { return Chronic.SeedingAge; }
-        }
-
-        
-        public double THC {
-            get { return Chronic.THC; }
-        }
-
-        public double Trimmings {
-            get { return Chronic.Trimmings; }
-        }
-
-        public Water Water {
-            get { return Chronic.Water; }
-        }
-
-        public Stage Stage
-        {
-            get { return Chronic.Stage; }
-        }
-
-        public Food Food
-        {
-            get { return Chronic.Food; }
-        }
-
-        public Light Light
-        {
-            get { return Chronic.Light; }
-        }
-
-        public Dictionary<Stage, Water> WaterNeed {
-            get { return Chronic.WaterNeed; }
-        }
-
-        public Dictionary<Stage, Light> LightNeed
-        {
-            get { return Chronic.LightNeed; }
-        }
-
-        public double Yield {
-            get { return Chronic.Yield; }
-
-            set { Chronic.Yield = value; }
-        }
-
         public event EventHandler Died;
 
+        // Refer to IChronic's clone function
         public IChronic Clone()
         {
             return Chronic.Clone();
         }
 
-        public IChronic Cure(IContainer container)
-        {
-            throw new InvalidOperationException(
-                "Cannot Cure a CannaOil in this state. CannaOil is a finalized product.");
-        }
-
-        public IChronic Dry()
-        {
-            throw new InvalidOperationException(
-                "Cannot Dry a CannaOil in this state. CannaOil is a finalized product.");
-        }
-
-        public IChronic Finish()
-        {
-            throw new InvalidOperationException(
-                "Cannot Finish a CannaOil in this state. CannaOil is a finalized product.");
-        }
-
-        public IChronic Grow(Water water, Light light, Food food)
-        {
-            throw new InvalidOperationException(
-                "Cannot Grow a CannaOil in this state. CannaOil is a finalized product.");
-        }
-
-        public IChronic Harvest()
-        {
-            throw new InvalidOperationException(
-                "Cannot Harvest a CannaOil in this state. CannaOil is a finalized product.");
-        }
-
-        public ISolvent Heat()
-        {
-            throw new InvalidOperationException(
-                "Cannot Heat a CannaOil in this state. CannaOil is a finalized product.");
-        }
-
+        // Refer to IChronic's print function
         public void Print()
         {
-            throw new NotImplementedException();
+            Chronic.Print();
         }
 
+        // Set Stage of IChronic
         public void SetStage(Stage stage)
         {
-            throw new NotImplementedException();
-        }
-
-        public IChronic Weck()
-        {
-            throw new InvalidOperationException(
-                "Cannot Weck a CannaOil in this state. CannaOil is a finalized product.");
+            Chronic.SetStage(stage);
         }
     }
 }
---
+++ GanjaLibrary/Classes/Oils/CannaOilVariables.cs
@@ -0,0 +1,114 @@
+﻿using GanjaLibrary.Enums;
+using System.Collections.Generic;
+
+namespace GanjaLibrary.Classes.Oils
+{
+    partial class CannaOil
+    {
+        public int Age
+        {
+            get { return Chronic.Age; }
+        }
+
+        public double CBD
+        {
+            get { return Chronic.CBD; }
+        }
+
+        public int DryingAge
+        {
+            get { return Chronic.DryingAge; }
+        }
+
+        public int FloweringAge
+        {
+            get { return Chronic.FloweringAge; }
+        }
+
+        public double Health
+        {
+            get { return Chronic.Health; }
+        }
+
+        public int MaxHealth
+        {
+            get { return Chronic.MaxHealth; }
+        }
+
+        public double MaxYield
+        {
+            get { return Chronic.MaxYield; }
+        }
+
+        public string Name
+        {
+            get { return Chronic.Name; }
+        }
+
+        public int SeedingAge
+        {
+            get { return Chronic.SeedingAge; }
+        }
+
+        public double THC
+        {
+            get { return Chronic.THC; }
+        }
+
+        public double Trimmings
+        {
+            get { return Chronic.Trimmings; }
+        }
+
+        public double Height
+        {
+            get { return Chronic.Height; }
+
+            set { Chronic.Height = value; }
+        }
+
+        public double Quality
+        {
+            get { return Chronic.Quality; }
+
+            set { Chronic.Quality = value; }
+        }
+
+        public double Yield
+        {
+            get { return Chronic.Yield; }
+
+            set { Chronic.Yield = value; }
+        }
+
+        public Water Water
+        {
+            get { return Chronic.Water; }
+        }
+
+        public Stage Stage
+        {
+            get { return Chronic.Stage; }
+        }
+
+        public Food Food
+        {
+            get { return Chronic.Food; }
+        }
+
+        public Light Light
+        {
+            get { return Chronic.Light; }
+        }
+
+        public Dictionary<Stage, Water> WaterNeed
+        {
+            get { return Chronic.WaterNeed; }
+        }
+
+        public Dictionary<Stage, Light> LightNeed
+        {
+            get { return Chronic.LightNeed; }
+        }
+    }
+}
